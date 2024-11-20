b6bfc8f82ca19e6fa43955abf8fdbf98bd00352e
See Yassine <seeyassine@gmail.com>
2024-11-21 00:36:57 +0100

    update

--- Chercheur-Service/src/main/java/dcc/tp2/chercheurservice/client/EnseignantRestFeign.java
+++ Chercheur-Service/src/main/java/dcc/tp2/chercheurservice/client/EnseignantRestFeign.java
@@ -2,7 +2,7 @@ package dcc.tp2.chercheurservice.client;
 
 
 import dcc.tp2.chercheurservice.module.Enseignant;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
